212af5fcb6b18e66883a945704fa90471c848a3f
2300032117 <2300032117@kluniversity.in>
2025-08-07 16:27:28 +0530

    Initial Commit

---
+++ src/MyProfile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+ class MyProfile extends Component {
+  render() {
+    return (
+      <div><h2>MyProfile</h2></div>
+    )
+  }
+}
+
+export default MyProfile;
